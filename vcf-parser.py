cd132026cf434f125222e0fc6f59edc6414c5a8b
LucHengeveld <luchengeveld@hotmail.com>
2022-04-24 22:21:23 +0200

    Added .idea / .vcf / .tsv to gitignore and created clinvar to tsv parser

--- vcf-parser.py
+++ vcf-parser.py
@@ -1,42 +1,83 @@
-"""
-This module saves the variants in the vcf file to a list.
-"""
+import csv
 
 
-def vcf_to_list(vcf_file_name):
-    """
-    Saves the variants in the vcf file to a list.
-    :param vcf_file_name: Name of the entered vcf file
-    :return vcf_list: List with the structure [CHROM, POS, ID, REF, ALT],
-        Might have extra columns added like QUAL, FILTER, INFO depending on the
-        entered vcf file.
+def clinvar_vcf_to_tsv(clinvar_vcf_file_name):
     """
 
-    # Creates an empty list
-    vcf_list = []
+    :param :
+    :return :
+    """
 
     # Loops through the vcf file line by line
-    with open(vcf_file_name) as file:
-        for line in file:
-
+    with open(clinvar_vcf_file_name) as vcf_file:
+        vcf_list = []
+        for line in vcf_file:
+            templist = []
             # If the line does not start with an # it appends the
             # variant information to a list
             if not line.startswith("#"):
+                templist.append(line.split("\t")[0])  # Chromosome
+                templist.append(line.split("\t")[1])  # Position
+                templist.append(line.split("\t")[3])  # REF
+                templist.append(line.split("\t")[4])  # ALT
 
-                # Checks if the line starts with chr or M\t, incase
-                # a different chromosome column format was used
-                if line.startswith("chr"):
-                    vcf_list.append(line.replace("chr", "", 1).split("\t"))
-                elif line.startswith("M\t"):
-                    vcf_list.append(line.replace("M", "MT", 1).split("\t"))
+                info_col = line.split("\t")[7]
+
+                # Gene name + ID (format name:id,
+                # (When it contains multiple genes: name1:id1|name2:id2)
+                if "GENEINFO=" in info_col:
+                    templist.append(
+                        info_col.split("GENEINFO=")[1].split(";")[0])
                 else:
-                    vcf_list.append(line.split("\t"))
+                    templist.append("null")
+
+                templist.append("Wild_type")  # Wild type?
+
+                # Variant type
+                if "MC=" in info_col:
+                    templist.append(
+                        info_col.split("MC=")[1].split(";")[0].split("|")[1])
+                else:
+                    templist.append("null")
+
+                # Clinical significance
+                if "CLNSIG=" in info_col:
+                    templist.append(info_col.split("CLNSIG=")[1].split(";")[0])
+                else:
+                    templist.append("null")
+
+                # Review status
+                if "CLNREVSTAT=" in info_col:
+                    templist.append(
+                        info_col.split("CLNREVSTAT=")[1].split(";")[0])
+                else:
+                    templist.append("null")
+
+                vcf_list.append(templist)
+
+            # Loops through first x amount of  variants
+            if len(vcf_list) == 20000:
+                break
+
+    vcf_file.close()
+
+    with open("clinvar_output.tsv", 'wt') as out_file:
+        tsv_writer = csv.writer(out_file, delimiter='\t')
+        tsv_writer.writerow(["Chromosome", "Position", "REF", "ALT",
+                             "Gene_name", "Wild_type?", "Variant_type",
+                             "Clinical_significance", "Review_status"])
+        for variant in vcf_list:
+            tsv_writer.writerow(variant)
 
     # Closes the file
-    file.close()
+    out_file.close()
 
-    # Sorts the list by chromosome
-    vcf_list.sort()
 
-    # Returns the list
-    return vcf_list
+def main():
+    clinvar_vcf_file_name = "clinvar.vcf"
+    vcf_file_name = "gnomad.genomes.v3.1.2.sites.chrY.vcf"
+
+    clinvar_vcf_to_tsv(clinvar_vcf_file_name)
+
+
+main()
