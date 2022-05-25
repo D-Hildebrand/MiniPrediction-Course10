b4fe4138e52b8730ac2b292e66863682fc2a7d51
LucHengeveld <luchengeveld@hotmail.com>
2022-05-25 22:41:43 +0200

    Added comments and docstrings

--- machine_learning_model.py
+++ machine_learning_model.py
@@ -1,22 +1,16 @@
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 from sklearn import metrics
-import numpy as np
 import re
 import blosum as bl
 
 
-def file_reader(file):
-    """
-    Converts a TSV file to a pandas dataframe
-    :param file: Input .TSV file
-    :return df: Pandas dataframe
-    """
-    return pd.read_table(file)
-
-
 def three_to_one(three_letter_code):
+    """
+    Converts the 3 letter protein codes to the corresponding 1 letter code
+    :param three_letter_code: 3 lettered protein code
+    :return: 1 letter protein code
+    """
     aa_list = {'CYS': 'C', 'ASP': 'D', 'SER': 'S', 'GLN': 'Q', 'LYS': 'K',
                'ILE': 'I', 'PRO': 'P', 'THR': 'T', 'PHE': 'F', 'ASN': 'N',
                'GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W',
@@ -25,8 +19,12 @@ def three_to_one(three_letter_code):
 
 
 def create_parq_df(input_parq):
+    """
+    Creates a pandas dataframe out of the parq file
+    :param input_parq: Parq files provided by BioProdict
+    :return df: Pandas dataframe containing information from the parq files
+    """
     df = pd.read_parquet(input_parq, engine='pyarrow')
-    df_rownames = df.index.values
     for i, row in df.iterrows():
         aa = re.split("[0-9]+", i)
         ref = three_to_one(aa[1])
@@ -38,30 +36,35 @@ def create_parq_df(input_parq):
 
 
 def parq_model(df_train, df_valid):
-    x_train = df_train[['conservationPro', 'conservationAla', 'conservationHis',
-               'conservationThr', 'conservationGln', 'conservationTyr',
-               'conservationGly', 'conservationArg', 'conservationVal',
-               'consWildType', 'conservationGlu', 'conservationMet',
-               'conservationLys', 'conservationIle', 'conservationPhe',
-               'conservationLeu', 'conservationAsn', 'conservationSer',
-               'conservationAsp', 'conservationCys', 'consVariant',
-               'conservationTrp', 'score']]
-    y_train = df_train['class']
+    """
+    Creates the random forest classifier
+    :param df_train: Dataframe with training parq data + blosum scores
+    :param df_valid: Dataframe with validation parq data + blosum scores
+    :return clf: Random forest classifier created by using the training and
+                 validation sets
+    """
+    df_columns = ['conservationPro', 'conservationAla', 'conservationHis',
+         'conservationThr', 'conservationGln', 'conservationTyr',
+         'conservationGly', 'conservationArg', 'conservationVal',
+         'consWildType', 'conservationGlu', 'conservationMet',
+         'conservationLys', 'conservationIle', 'conservationPhe',
+         'conservationLeu', 'conservationAsn', 'conservationSer',
+         'conservationAsp', 'conservationCys', 'consVariant',
+         'conservationTrp', 'score']
 
-    x_valid = df_valid[['conservationPro', 'conservationAla', 'conservationHis',
-               'conservationThr', 'conservationGln', 'conservationTyr',
-               'conservationGly', 'conservationArg', 'conservationVal',
-               'consWildType', 'conservationGlu', 'conservationMet',
-               'conservationLys', 'conservationIle', 'conservationPhe',
-               'conservationLeu', 'conservationAsn', 'conservationSer',
-               'conservationAsp', 'conservationCys', 'consVariant',
-               'conservationTrp', 'score']]
+    # Retrieves the train and validation information and labels
+    x_train = df_train[df_columns]
+    y_train = df_train['class']
+    x_valid = df_valid[df_columns]
     y_valid = df_valid['class']
 
+    # Creates the random forest classifier
     clf = RandomForestClassifier(n_estimators=100)
 
+    # Fits the model
     clf.fit(x_train, y_train)
 
+    # Predicts the pathogenicity
     y_pred = clf.predict(x_valid)
 
     print("Accuracy:", metrics.accuracy_score(y_valid, y_pred))
@@ -69,6 +72,14 @@ def parq_model(df_train, df_valid):
 
 
 def clf_test(df_test, clf):
+    """
+    Classifier that predicts the pathogenicity of the test data
+    :param df_test: Dataframe with test parq data + blosum scores
+    :param clf: Random forest classifier created by using the training and
+           validation sets
+    :return test_data_prediction.tsv: File with predictions added to the test
+            data
+    """
     x_test = df_test[
         ['conservationPro', 'conservationAla', 'conservationHis',
          'conservationThr', 'conservationGln', 'conservationTyr',
@@ -78,38 +89,26 @@ def clf_test(df_test, clf):
          'conservationLeu', 'conservationAsn', 'conservationSer',
          'conservationAsp', 'conservationCys', 'consVariant',
          'conservationTrp', 'score']]
-    y_pred = clf.predict(x_test)
-    df_test['class'] = y_pred.tolist()
-    df_test.to_csv('test_data_predictions.tsv', sep="\t")
 
-
-def blosum_classifier(df):
-    blosum = np.vstack(df['Score'])
-    pathogenicity = df['Label']
-
-    x_train, x_test, y_train, y_test = train_test_split(blosum, pathogenicity,
-                                                        test_size=0.25)
-
-    clf = RandomForestClassifier(n_estimators=100)
-
-    clf.fit(x_train, y_train)
-
+    # Predicts the pathogenicity
     y_pred = clf.predict(x_test)
 
-    print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+    # Adds the prediction to the output file
+    y_pred.tolist().to_csv('test_data_predictions.tsv', sep="\t")
 
 
 def main():
+    # Creates the train, validation and test pandas dataframes
     df_train = create_parq_df("train_data_bio_prodict.parq")
     df_valid = create_parq_df("valid_data_bio_prodict.parq")
     df_test = create_parq_df("test_data_bio_prodict.parq")
+
+    # Creates the random forest classifier
     clf = parq_model(df_train, df_valid)
+
+    # Uses the random forest classifier to predict the data from the
+    # test set
     clf_test(df_test, clf)
-
-    # file = "train_data_bio_prodict.tsv"
-    # df = file_reader(file)
-    # blosum_classifier(df)
-
 
 
 main()
